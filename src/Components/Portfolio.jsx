2c7d6108e1f3d15c4f0bae956095d820473b8a60
Ahmed Mustafa <87272564+AhmedMustafa249@users.noreply.github.com>
2024-05-24 17:22:12 +0000

    ANIMATION

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -22,7 +22,6 @@ import React from "react";
 import image from "../images/design-desk.jpeg";
 
 const imageAltText = "desktop with books and laptop";
-
 /**
  * Project list
  *
@@ -64,7 +63,12 @@ const Portfolio = () => {
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            style={{
+              height: "90%",
+              width: "100%",
+              objectFit: "cover",
+              animation: "1s ease-out 0s 1 slideInLeft",
+            }}
             alt={imageAltText}
           />
         </div>
