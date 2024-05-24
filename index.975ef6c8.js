99bf5ce23b966c01d5fdd6b329c72299cb3aac15
Ahmed Mustafa <87272564+AhmedMustafa249@users.noreply.github.com>
2024-05-24 16:54:02 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27087,7 +27087,7 @@ var _stylesCss = require("./styles.css");
  *
  * If you don't have one of the social sites listed, leave it as an empty string.
  */ const siteProps = {
-    name: "Nigga Fr",
+    name: "Nigga Fr Ong",
     title: "Web Designer & Content Creator",
     email: "alex@example.com",
     gitHub: "microsoft",
